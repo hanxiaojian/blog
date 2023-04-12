851d3729bf9a0f036c92000e2765ea5b8bd40b27
hanxiaojian <3099496620@qq.com>
2023-04-12 15:22:37 +0800

    新增修改个人信息后保存功能

--- blog-vue/src/main/java/com/example/controller/UserController.java
+++ blog-vue/src/main/java/com/example/controller/UserController.java
@@ -1,12 +1,11 @@
 package com.example.controller;
 
 import com.example.domain.ResponseResult;
+import com.example.domain.entity.User;
 import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.web.servlet.oauth2.resourceserver.OAuth2ResourceServerSecurityMarker;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -17,5 +16,10 @@ public class UserController {
     @GetMapping("/userInfo")
     public ResponseResult userInfo() {
         return userService.userInfo();
+    }
+
+    @PostMapping("/userInfo")
+    public ResponseResult updateUserInfo(@RequestBody User user) {
+        return userService.updateUserInfo(user);
     }
 }
